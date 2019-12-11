09de1d9667d1aaa97ed83a29d470b77f29f6d666
Dilan Morar <DMorar@spartaglobal.com>
2019-12-11 16:08:21 +0000

    adding more code to get request file

--- get_requests.py
+++ get_requests.py
@@ -7,3 +7,17 @@ arguements = 'e146gt'
 post_codes = requests.get(path_url+arguements)
 
 print(post_codes)
+print(type(post_codes))
+
+# turn this into a dictionary
+dict_response = post_codes.json()
+print(dict_response)
+
+# getting out data
+print(dict_response.keys())
+print(dict_response['status'])
+print(dict_response['result'].keys())
+print(dict_response['result']['longitude'])
+
+for key in dict_response:
+    print (key, ' ', dict_response['result'][key])
