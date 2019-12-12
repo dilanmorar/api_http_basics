30b4dfa605c527b6211e77646ceca0db0e796ef9
Dilan Morar <DMorar@spartaglobal.com>
2019-12-12 20:45:42 +0000

    function for returning the longitude

--- task.py
+++ task.py
@@ -15,17 +15,24 @@ print(dict_response['status'])
 #This prints out the results as a dictionary
 print(dict_response['result'])
 
-# 2 - from a postcode retrieve: lonitude, latitude, nuts, admin ward
-# This prints out the longitude, latitude, nuts and admin_ward
+# 2 - prints out the longitude, latitude, nuts and admin_ward of a postcode
 print('longitude: ' + dict_response['result']['longitude'])
 print('latitude: ' + dict_response['result']['latitude'])
 print('nuts: ' + dict_response['result']['nuts'])
 print('admin_ward' + dict_response['result']['admin_ward'])
 
-# 3 - build a function that returns a lat of a postcode
-def postcode_lat():
+# 3 - function that returns the latitude of a postcode
+def postcode_latitude():
     user_postcode = input('Input: ')
     path_url = 'http://api.postcodes.io/postcodes/'
     result = requests.get(path_url + user_postcode.strip())
     post_code_dict = result.json()
     return post_code_dict['result']['latitude']
+
+# 4 - function that returns the longitude of a postcode
+def postcode_longitude():
+    user_postcode = input('Input: ')
+    path_url = 'http://api.postcodes.io/postcodes/'
+    result = requests.get(path_url + user_postcode.strip())
+    post_code_dict = result.json()
+    return post_code_dict['result']['longitude']
